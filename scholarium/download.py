b75b682d5ef6bfea267a61a3794e99e74fb37f45
Philipp Bogensberger <philipp.bogensberger@gmail.com>
2018-08-16 12:17:48 +0200

    fetch list of available scholien

--- scholarium/download.py
+++ scholarium/download.py
@@ -1,9 +1,20 @@
 import argparse
 import requests
 
+from bs4 import BeautifulSoup
+
 LOGIN_URL = 'https://scholarium.at/nutzer/anmelden/'
 
-BASIC_URL = 'https://scholarium.at/warenkorb/bestellungen'
+ORDERS_URL = 'https://scholarium.at/warenkorb/bestellungen'
+
+
+class ScholieSource:
+
+    def __init__(self, soup):
+        self.soup = soup
+
+    def title(self):
+        return self.soup.find(class_='bestellung_titel').find('a').text
 
 
 class ScholariumWebpage:
@@ -21,6 +32,21 @@ class ScholariumWebpage:
                     'password': password
                 })
 
+    def fetch_available_scholien(self):
+        res = self.session.get(ORDERS_URL)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        content_tables = soup.find_all('table', class_='bestellung_item')
+        sources = []
+        for table in content_tables:
+            for tr in table.find_all('tr'):
+                if tr.find(text='Büchlein'):
+                    source = ScholieSource(tr)
+                    sources.append(source)
+        return sources
+
+    def download(self, source):
+        pass
+
     def _fetch_csrftoken(self):
         res = self.session.get('https://scholarium.at')
         return res.cookies.get('csrftoken')
@@ -35,6 +61,13 @@ def main():
     args = parser.parse_args()
     page = ScholariumWebpage()
     page.login(args.email, args.password)
+    print('Looking for available Scholien')
+    scholien_sources = page.fetch_available_scholien()
+    if len(scholien_sources) > 0:
+        print('Found:')
+        for src in scholien_sources:
+            print(src.title())
+    page.download(scholien_sources)
 
 
 if __name__ == '__main__':

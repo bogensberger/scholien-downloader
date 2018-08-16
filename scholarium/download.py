f527acc962546c486cf0e9b3948acfb9f263d7b7
Philipp Bogensberger <philipp.bogensberger@gmail.com>
2018-08-16 13:44:04 +0200

    implement actual download

--- scholarium/download.py
+++ scholarium/download.py
@@ -1,11 +1,17 @@
 import argparse
 import requests
 
+from os.path import join, abspath
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-LOGIN_URL = 'https://scholarium.at/nutzer/anmelden/'
+BASE_URL = 'https://scholarium.at'
+LOGIN_URL = urljoin(BASE_URL, '/nutzer/anmelden/')
 
-ORDERS_URL = 'https://scholarium.at/warenkorb/bestellungen'
+ORDERS_URL = urljoin(BASE_URL, '/warenkorb/bestellungen')
+
+
+DEFAULT_TARGET_DIR = join(abspath('.'), 'downloads')
 
 
 class ScholieSource:
@@ -16,9 +22,24 @@ class ScholieSource:
     def title(self):
         return self.soup.find(class_='bestellung_titel').find('a').text
 
+    def filename(self, base_path=None):
+        if base_path is None:
+            base_path = DEFAULT_TARGET_DIR
+        name = '{}.mobi'.format(self.title().lower())
+        name = name.replace('/', '_').replace(' ', '_')
+        return join(base_path, name)
+
+    def form_data(self):
+        form = self.soup.find('form')
+        data = {
+            input.get('name'): input.get('value')
+            for input in form.find_all('input')
+            if input.get('name')
+        }
+        return form.get('action'), data
+
 
 class ScholariumWebpage:
-
     def __init__(self):
         self.session = requests.session()
 
@@ -39,16 +60,24 @@ class ScholariumWebpage:
         sources = []
         for table in content_tables:
             for tr in table.find_all('tr'):
-                if tr.find(text='Büchlein'):
+                if tr.find(text='Büchlein') and tr.find(text='Mobi'):
                     source = ScholieSource(tr)
                     sources.append(source)
         return sources
 
-    def download(self, source):
-        pass
+    def download(self, sources):
+        for source in sources:
+            action, data = source.form_data()
+            url = urljoin(BASE_URL, action)
+            print("Downloading {}".format(source.filename()))
+            res = self.session.post(url, data)
+            with open(source.filename(), 'wb') as f:
+                for chunk in res.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
 
     def _fetch_csrftoken(self):
-        res = self.session.get('https://scholarium.at')
+        res = self.session.get(BASE_URL)
         return res.cookies.get('csrftoken')
 
 

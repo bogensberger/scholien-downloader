10aa5ab5506b4b04e5338ff1c05fe7dbdba6c284
Philipp Bogensberger <philipp.bogensberger@gmail.com>
2018-08-16 11:06:48 +0200

    added setup files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'beautifulsoup4'
+]
+
+setup(
+    name='scholarium.analyzer',
+    version='0.0.1',
+    author='Philipp Bogensberger',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    extras_require={
+        'test': []
+    },
+    entry_points={
+        'console_scripts': [
+            'download=scholarium.download:main',
+        ]
+    }
+)
